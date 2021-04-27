c96c2621e48711f9c059e9b3c19a2faca19d8ca6
Ray Chung <ray20020809@gmail.com>
2021-04-27 14:49:34 +0800

    update (rewrite heapsort myself)

--- hw02/heapsort.c
+++ hw02/heapsort.c
@@ -1,45 +1,69 @@
 #include "yah.h"
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 
-static void swp(int *a, int *b) {
-	int temp = *b;
-	*b = *a;
-	*a = temp;
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+int idx = 0;
+static inline int left(int i) {return 2 * i + 1 ;}
+static inline int right(int i) {return 2 * i + 2;}
+static inline int parent (int i) {assert(i>0); return (i-1) / 2;}
+
+static inline void swap(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-static void max_heapify(int arr[], int start, int end) {
-	// 建立父節點指標和子節點指標
-	int dad = start;
-	int son = dad * 2 + 1;
-	while (son <= end) { // 若子節點指標在範圍內才做比較
-		if (son + 1 <= end && arr[son] < arr[son + 1]) // 先比較兩個子節點大小，選擇最大的
-			son++;
-		if (arr[dad] > arr[son]) //如果父節點大於子節點代表調整完畢，直接跳出函數
-			return;
-		else { // 否則交換父子內容再繼續子節點和孫節點比较
-			swp(&arr[dad], &arr[son]);
-			dad = son;
-			son = dad * 2 + 1;
-		}
-	}
+void downSwap(int heap[], int i) {
+    if (i*2+1 >= idx) return;
+    // printf("down! p: %d, l: %d, r: %d\n", heap[i], heap[left(i)], heap[right(i)]);
+    if (right(i) >= idx) {
+        swap(&heap[i], &heap[left(i)]);
+    };
+    if (heap[i] > heap[left(i)] && heap[i] > heap[right(i)]) return;
+    if (heap[left(i)] > heap[right(i)]) {
+        swap(&heap[i], &heap[left(i)]);
+        downSwap(heap, left(i));
+    } else {
+        swap(&heap[i], &heap[right(i)]);
+        downSwap(heap, right(i));
+    }
 }
 
-static void heap_sort(int arr[], int len) {
-	int i;
-	// 初始化，i從最後一個父節點開始調整
-	for (i = len / 2 - 1; i >= 0; i--)
-		max_heapify(arr, i, len - 1);
-	// 先將第一個元素和已排好元素前一位做交換，再重新調整，直到排序完畢
-	for (i = len - 1; i > 0; i--) {
-		swp(&arr[0], &arr[i]);
-		max_heapify(arr, 0, i - 1);
-	}
+static inline int pop(int heap[]) {
+    assert(idx > 0);
+    int r = heap[0];
+    if (idx == 1) {idx = 0; return r;}
+    idx--;
+    heap[0] = heap[idx];
+    downSwap(heap, 0);
+    return r;
 }
 
-void heapsort(int arr[]) {
-	heap_sort(arr, LEN);
+static inline void push(int heap[], int v) {
+    // printf("push[%d] %d\n", idx, v);
+    int i = idx;
+    while (i > 0 && heap[parent(i)] < v) {
+        heap[i] = heap[parent(i)];
+        i = parent(i);
+    }
+    heap[i] = v;
+    idx++;
+
 }
+
+void heapsort(int heap[]) {
+    srand(time(0));
+    for (int i = 0; i < LEN; i++) {
+        push(heap, heap[i]);
+    }
+    for (int i = 0 ; i < LEN; i++) {
+        heap[LEN - i - 1] = pop(heap);
+    }
+}
+
