cfd821ab1fb1d65fe0f380464a1982e54e26c179
Ray1422 <ray20020809@gmail.com>
2021-04-28 21:24:03 +0800

    add pthread quicksort

--- hw02/quicksort.c
+++ hw02/quicksort.c
@@ -1,51 +1,62 @@
+#include <pthread.h>
+
 #include "yah.h"
 
+struct args {
+    int *data;
+    int left;
+    int right;
+};
 static void swp(int *a, int *b) {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
-static void quicksort_t(int *data, int left, int right); 
+static void *quicksort_c(void *);
 
 void quicksort(int arr[]) {
-	quicksort_t(arr, 0, LEN - 1);
+    quicksort_c(&(struct args){.data = arr, .left = 0, .right = LEN - 1});
 }
 
+static void *quicksort_c(void *v_args) {
+    struct args *args = v_args;
+    int *data = args->data, left = args->left, right = args->right;
+    int pivot, i, j;
+    if (left >= right) {
+        return NULL;
+    }
+    pivot = data[left];
+    i = left + 1;
+    j = right;
+    while (1) {
+        while (i <= right) {
+            if (data[i] > pivot) {
+                break;
+            }
+            i = i + 1;
+        }
 
-static void quicksort_t(int *data, int left, int right) {
-	int pivot, i, j;
-	if (left >= right) { return; }
-	pivot = data[left];
-
-	i = left + 1;
-	j = right;
-
-	while (1) {
-		while (i <= right) {
-			if (data[i] > pivot) {
-				break;
-			}
-
-			i = i + 1;
-		}
-
-		while (j > left) {
-			if (data[j] < pivot) {
-				break;
-			}
-
-			j = j - 1;
-		}
-
-		if (i > j) { break; }
-
-		swp(&data[i], &data[j]);
-	}
-
-	swp(&data[left], &data[j]);
-
-	quicksort_t(data, left, j - 1);
-	quicksort_t(data, j + 1, right);
+        while (j > left) {
+            if (data[j] < pivot) {
+                break;
+            }
+            j = j - 1;
+        }
+        if (i > j) {
+            break;
+        }
+        swp(&data[i], &data[j]);
+    }
+    swp(&data[left], &data[j]);
+    if (right - left > 125000) {
+        pthread_t p1, p2;
+        pthread_create(&p1, NULL, quicksort_c, &(struct args){.data = data, .left = left, .right = j - 1});
+        pthread_create(&p2, NULL, quicksort_c, &(struct args){.data = data, .left = j + 1, .right = right});
+        pthread_join(p1, NULL);
+        pthread_join(p2, NULL);
+    } else {
+        quicksort_c(&(struct args){.data = data, .left = left, .right = j - 1});
+        quicksort_c(&(struct args){.data = data, .left = j + 1, .right = right});
+    }
+    return NULL;
 }
-
-
--- hw02/yah.h
+++ hw02/yah.h
@@ -3,7 +3,8 @@
 #define YAH_H
 
 // #define LEN 10
-#define LEN 10000000
+// #define LEN 10000000
+#define LEN 1000000000 // 1e9
 void mergesort(int a[]);
 void quicksort(int a[]);
 void heapsort(int a[]);
